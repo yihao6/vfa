73bbd8b924bae9c943b4baf87c82a7c605034bee
yihao6 <yihao6.2018@gmail.com>
2025-04-22 13:09:01 -0400

    Switch to setup.cfg for pip

--- setup.py
+++ setup.py
@@ -1,53 +1,8 @@
-from setuptools import setup, find_packages
-import os
+import setuptools
+from pathlib import Path
 
-__package_name__ = "vfa"
+# Dynamically read requirements.txt
+here = Path(__file__).parent
+requirements = here.joinpath("requirements.txt").read_text().splitlines()
 
-def get_version_and_cmdclass(pkg_path):
-    """Load version.py module without importing the whole package.
-
-    Template code from miniver
-    """
-    from importlib.util import module_from_spec, spec_from_file_location
-
-    spec = spec_from_file_location("version", os.path.join(pkg_path, "_version.py"))
-    module = module_from_spec(spec)
-    spec.loader.exec_module(module)
-    return module.__version__, module.get_cmdclass(pkg_path)
-
-
-__version__, cmdclass = get_version_and_cmdclass(__package_name__)
-
-
-with open('requirements.txt') as f:
-    install_requires = f.read().splitlines()
-
-# noinspection PyTypeChecker
-setup(
-    name=__package_name__,
-    version=__version__,
-    description="Vector Field Attention",
-    long_description="Vector Field Attention for Deformable Image Registration",
-    author="Yihao Liu",
-    author_email="yliu236@jhu.edu",
-    url="https://gitlab.com/yihao6/vfa",
-    license="GNU General Public License v3.0",
-    classifiers=[
-        "Development Status :: 3 - Alpha",
-        "Environment :: Console",
-        "Intended Audience :: Science/Research",
-        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
-        "Programming Language :: Python :: 3.8",
-        "Topic :: Scientific/Engineering",
-    ],
-    packages=find_packages(),
-    include_package_data=True,
-    keywords="surfaces reconstruction",
-    entry_points={
-        "console_scripts": [
-            "vfa-run=vfa.main:main",
-        ]
-    },
-    install_requires=install_requires,
-    cmdclass=cmdclass,
-)
+setuptools.setup(install_requires=requirements)
---
+++ vfa/__init__.py
@@ -0,0 +1,10 @@
+"""Top-level package for vfa."""
+
+__title__ = "vfa"
+__description__ = "VFA: Vecor Field Attention"
+__url__ = "https://github.com/yihao6/vfa"
+__author__ = """Yihao Liu"""
+__email__ = "yliu236@jhu.edu"
+__version__ = "1.1.2"
+__license__ = "GPLv3"
+__copyright__ = "Copyright 2023 Yihao Liu"
